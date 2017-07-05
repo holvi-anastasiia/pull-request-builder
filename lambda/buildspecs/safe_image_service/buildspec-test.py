7d420df0ba54c8e818f27fb02b1d885254e6e11a
Anastasiia Diudina <anastasiia@holvi.com>
2017-07-05 16:48:41 +0300

    Add configs for prod lambda

---
+++ lambda/buildspecs/safe_image_service/buildspec-test.py
@@ -0,0 +1,19 @@
+YAML_CONFIG = """version: 0.2
+
+env:
+  variables:
+    LAMBDA: "default"
+    SNS_TOPIC_ARN: arn:aws:sns:eu-west-1:890057277545:LambdaDeployPipeline
+phases:
+  install:
+    commands:
+      - pip install -r requirements.txt
+  pre_build:
+    commands:
+      - 'cd lambdas/$LAMBDA && pip install -r requirements.txt'
+  build:
+    commands:
+      - 'coverage run --source=. -m unittest discover'
+  post_build:
+    commands:
+      - 'aws sns publish --topic-arn arn:aws:sns:eu-west-1:890057277545:LambdaDeployPipeline --message "{\\"buildId\\": \\"$CODEBUILD_BUILD_ID\\"}"'"""
